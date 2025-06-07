640d913246de9196dbc3d7416f6e17a8b4b26780
Siniclei102 <fialhoedgabrill@gmail.com>
2025-06-07 12:09:07 -0300

    Delete postadorfacebook.painelcontrole.xyz directory

--- postadorfacebook.painelcontrole.xyz/config/facebook-app.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configurações da API do Facebook
-define('FB_APP_ID', 'SEU_APP_ID_FACEBOOK');
-define('FB_APP_SECRET', 'SEU_APP_SECRET_FACEBOOK');
-define('FB_APP_VERSION', 'v18.0');
-define('FB_REDIRECT_URI', 'https://seusite.com/api/facebook-callback.php');
-define('FB_PERMISSIONS', ['public_profile', 'email', 'groups_access_member_info', 'publish_to_groups']);
-?>
