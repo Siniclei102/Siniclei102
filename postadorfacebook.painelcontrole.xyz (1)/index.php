c04e600c43e6ae586cc39e218023bb7349c472da
Siniclei102 <fialhoedgabrill@gmail.com>
2025-06-15 18:23:11 -0300

    Delete postadorfacebook.painelcontrole.xyz (1) directory

--- postadorfacebook.painelcontrole.xyz (1)/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Iniciar sessão
-session_start();
-
-// Redirecionar para a página de login
-header('Location: login.php');
-exit;
--- postadorfacebook.painelcontrole.xyz (1)/uploads/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Acesso negado
-header('HTTP/1.0 403 Forbidden');
-exit('Acesso negado');
-?>
